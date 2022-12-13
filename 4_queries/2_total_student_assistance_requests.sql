f83b2097ec7c6834a3207211c926afab0b5ddfb9
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2022-12-13 09:24:47 -0700

    complete 2_total_student_assistance_requests

---
+++ 4_queries/2_total_student_assistance_requests.sql
@@ -0,0 +1,5 @@
+SELECT COUNT(assistance_requests.*) as total_assistances, students.name
+FROM assistance_requests
+JOIN students ON students.id = assistance_requests.student_id
+WHERE name = 'Elliot Dickinson'
+GROUP BY students.name;
